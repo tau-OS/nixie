839418e2f2d0ffb64ea3d5fde8729aa2f6e0b84e
ItsJamie9494 <hello@jamiethalacker.dev>
2022-06-23 14:43:02 -0700

    Build scripts, schemas

--- build.rs
+++ build.rs
@@ -1,9 +1,39 @@
 use gio;
+use std::{
+    io::{stderr, stdout, Write},
+    process::Command,
+};
 
 fn main() {
+    println!("cargo:rerun-if-changed=data");
+
     gio::compile_resources(
         "data",
         "data/co.tauos.Nixie.gresources.xml",
         "co.tauos.Nixie.gresource",
     );
+
+    // TODO: See if there's a way to do this that won't rely on Command
+    let cpout = Command::new("sudo")
+        .arg("cp")
+        .arg("data/co.tauos.Nixie.gschema.xml")
+        .arg("/usr/share/glib-2.0/schemas/")
+        .output()
+        .expect("Failed to copy Schema");
+
+    let comout = Command::new("sudo")
+        .arg("glib-compile-schemas")
+        .arg("/usr/share/glib-2.0/schemas/")
+        .output()
+        .expect("Failed to compile Schemas");
+
+    if !cpout.status.success() {
+        stdout().write_all(&cpout.stdout).unwrap();
+        stderr().write_all(&cpout.stderr).unwrap();
+    }
+
+    if !comout.status.success() {
+        stdout().write_all(&comout.stdout).unwrap();
+        stderr().write_all(&comout.stderr).unwrap();
+    }
 }
